5f5a31b123bbbc5e679cfc484bd139601c41cd8b
Josephine Elder <parusmontanus@gmail.com>
2017-09-05 15:40:36 +0100

    Making JsonParser and adding gson

---
+++ app/src/main/java/com/josephineelder/marsroving/JsonParser.java
@@ -0,0 +1,21 @@
+package com.josephineelder.marsroving;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonParser {
+
+    Gson gson = new Gson();
+
+    public List<String> getRovers(String json) {
+        List<String> rovers = new ArrayList<>();
+
+        
+
+
+        return rovers;
+    }
+
+}
