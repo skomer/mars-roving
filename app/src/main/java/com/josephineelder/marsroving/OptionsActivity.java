973e3fb34919dd29e057247da045e695221920c1
Josephine Elder <parusmontanus@gmail.com>
2017-09-07 14:37:30 +0100

    Replacing rover names textview with spinner

--- app/src/main/java/com/josephineelder/marsroving/OptionsActivity.java
+++ app/src/main/java/com/josephineelder/marsroving/OptionsActivity.java
@@ -1,7 +1,11 @@
 package com.josephineelder.marsroving;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -9,15 +13,17 @@ import java.util.List;
 
 public class OptionsActivity extends AppCompatActivity implements OptionsActivityView {
 
-    TextView roverNames;
+    Spinner roversSpinner;
     OptionsActivityPresenter presenter;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        roverNames = (TextView) findViewById(R.id.roverNames);
+        context = this;
 
+        roversSpinner = (Spinner) findViewById(R.id.roversSpinner);
         presenter = new OptionsActivityPresenter(this, new JsonParser(), new HttpConnector());
     }
 
@@ -27,8 +33,15 @@ public class OptionsActivity extends AppCompatActivity implements OptionsActivit
         presenter.onResume();
     }
 
-    public void showRovers(List<String> rovers) {
-        roverNames.setText(rovers.toString());
+    public void showRovers(final List<String> rovers) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayAdapter adapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, rovers) {};
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                roversSpinner.setAdapter(adapter);
+            }
+        });
     }
 
     public void displayMessage(final String message) {
