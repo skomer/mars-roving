797fa77520d682f9960f9ecd3128fb5d69907748
Josephine Elder <parusmontanus@gmail.com>
2017-09-07 21:19:57 +0100

    Creating Camera class

---
+++ app/src/main/java/com/josephineelder/marsroving/Camera.java
@@ -0,0 +1,13 @@
+package com.josephineelder.marsroving;
+
+public class Camera {
+
+    public final String name;
+    public final String fullName;
+
+    public Camera(String name, String fullName) {
+        this.name = name;
+        this.fullName = fullName;
+    }
+
+}
